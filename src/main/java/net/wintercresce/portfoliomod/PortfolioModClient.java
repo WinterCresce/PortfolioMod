c9764dfab8e901ef321bb23bb18f8a72605e7dcb
WinterCresce <owainshorework2@gmail.com>
2025-06-20 15:04:32 +0100

    adding initial files

---
+++ src/main/java/net/wintercresce/portfoliomod/PortfolioModClient.java
@@ -0,0 +1,10 @@
+package net.wintercresce.portfoliomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PortfolioModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/wintercresce/portfoliomod/PortfolioModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.wintercresce.portfoliomod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PortfolioModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
