1fc4663450ba3ada45b6317e870ec068eeab01e4
WinterCresce <owainshorework2@gmail.com>
2025-06-21 16:12:59 +0100

    blood diamond item and resources not running again

--- src/main/java/net/wintercresce/portfoliomod/item/ModItems.java
+++ src/main/java/net/wintercresce/portfoliomod/item/ModItems.java
@@ -5,6 +5,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.wintercresce.portfoliomod.PortfolioMod;
 
@@ -12,9 +14,17 @@ public class ModItems {
 
     public static final Item BLOOD_DIAMOND = registerItem( "blood_diamond", new Item(new Item.Settings())) ;
 
-
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(PortfolioMod.MOD_ID, name), item);
+        return Registry.register(Registries.ITEM, Identifier.of("portfoliomod", name), item);
+    }
+
+
+    private static Item register(Item.Settings itemSettings, String name) {
+        Identifier id = Identifier.of(PortfolioMod.MOD_ID, name);
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+        Item.Settings settings = itemSettings.registryKey(key);
+
+        return Registry.register(Registries.ITEM, key, new Item(settings));
     }
 
     public static void registerModItems() {
